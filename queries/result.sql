3b1dfe63e7661033745f10da9f64c10bbc9fb04d
hebafer <hectorballega@gmail.com>
2021-05-11 21:47:53 +0200

    Added SQLFlow extended queries

--- queries/result.sql
+++ queries/result.sql
@@ -1,1 +1,2 @@
-SELECT image_id, file_name, person, bicycle, car, motorcycle, airplane, bus, train, truck, boat, result.`traffic light` FROM coco.`result`;
+SELECT image_id, file_name, person, bicycle, car, motorcycle, airplane, bus, train, truck, boat, result.`traffic light` 
+FROM coco.result;
